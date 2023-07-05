9a55ebc5314c0e1d5227c7e5f2788e9278647551
Arjun Yadav <yadarj120@gmail.com>
2023-07-05 12:40:13 +0530

    change footer and correct port for deployment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+require("dotenv").config();
 
 const app = express();
 
@@ -40,7 +41,7 @@ const List = mongoose.model("List", listSchema);
 
 async function connectDB(){
   try {
-    await mongoose.connect("mongodb://localhost:27017/todolistDB")
+    await mongoose.connect(`mongodb+srv://yadarj120:${process.env.PASSWORD}@cluster0.skmxkdd.mongodb.net/todolistDB`);
     console.log("connected successfully"); 
   } 
   catch(err) {
@@ -170,6 +171,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
